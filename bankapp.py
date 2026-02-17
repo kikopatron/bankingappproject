2a32a2545fc08aca4e38abd372da40a40c13368f
Eric Wright Jr. <ftacapitalfx@gmail.com>
2026-02-17 15:44:39 -0500

    added dict/pandas/tocsv

--- bankapp.py
+++ bankapp.py
@@ -1,6 +1,9 @@
+import pandas as pd
+
 class BankingApp():
     def __init__(self):
         self.BankingQuestions()
+        self.BankingTable()
         
     def BankingQuestions(self):
         self.q_name = "What's your name? "
@@ -11,11 +14,18 @@ class BankingApp():
     def BankBegin(self):
         self.name = input(self.q_name)
         self.BankingPrompt()
+        df = pd.DataFrame(self.BankingInfoTable)
+        df.to_csv("banking_trans.csv")
+        print(df.tail())
 
     def BankingPrompt(self):
+        
         self.deposit = float(input(self.q_deposit))
+        self.BankingInfoTable['Deposit'].append(self.deposit)
         self.withdrawal = float(input(self.q_withdrawal))
+        self.BankingInfoTable['Withdrawal'].append(self.withdrawal)
         self.BankingCalc()
+        self.BankingInfoTable['Balance'].append(self.endBalance)
         self.nxttran = input(self.anotherone)
         while self.nxttran == "Yes":
             self.BankingPrompt()
@@ -25,8 +35,17 @@ class BankingApp():
                 break
     
     def BankingCalc(self):
-        endBalance = self.deposit - self.withdrawal
-        print(f"{self.name}, you have ${endBalance} in your account!")
+        # self.BankingTable()
+        self.endBalance = self.deposit - self.withdrawal
+        print(f"{self.name}, you have ${self.endBalance} in your account!")
+    
+    def BankingTable(self):
+        self.BankingInfoTable = {
+            'Deposit':[],
+            'Withdrawal':[],
+            'Balance':[],
+            }
+
 
 bnk = BankingApp()
 bnk.BankBegin()
